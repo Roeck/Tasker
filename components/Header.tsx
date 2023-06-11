c5dafaa8949661dbb158e77de294765cc52d08f9
rons9n <rons9n@gmail.com>
2023-06-11 18:02:38 -0500

    Profile circle icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,6 +43,12 @@ function Header() {
           />
         </div>
       </div>
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1] p-5">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          AI is summarising for your day...
+        </p>
+      </div>
     </header>
   );
 }
