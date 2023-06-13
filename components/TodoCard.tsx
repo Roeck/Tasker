81d08aaba52eb458b58a8f4243e897d67d99278a
rons9n <rons9n@gmail.com>
2023-06-13 16:26:25 -0500

    Implement imageUrl function

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -4,9 +4,11 @@ import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
 } from "react-beautiful-dnd";
+import { useEffect, useState } from "react";
 
+import Image from "next/image";
 import { XCircleIcon } from "@heroicons/react/24/solid";
-
+import getUrl from "@/lib/getUrl";
 import { useBoardStore } from "@/store/BoardStore";
 
 type Props = {
@@ -27,7 +29,20 @@ function TodoCard({
   dragHandleProps,
 }: Props) {
   const deleteTask = useBoardStore((state) => state.deleteTask);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
+  useEffect(() => {
+    if (todo.image) {
+      const fetchImage = async () => {
+        const url = await getUrl(todo.image!);
+        if (url) {
+          setImageUrl(url.toString());
+        }
+      };
+
+      fetchImage();
+    }
+  }, [todo]);
   return (
     <div
       className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -46,6 +61,17 @@ function TodoCard({
       </div>
 
       {/* Add image here... */}
+      {imageUrl && (
+        <div className="h-full w-full rounded-b-md">
+          <Image
+            src={imageUrl}
+            alt=""
+            width={400}
+            height={200}
+            className="w-full object-contain rounded-b-md"
+          />
+        </div>
+      )}
     </div>
   );
 }
